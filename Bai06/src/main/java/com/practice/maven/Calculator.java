feed56c1ccc740c24ca77b5e768a17f149fa35f8
vhoang0502 <viethoang050207@gmail.com>
2026-05-06 22:23:44 +0700

    Feat: add Bai06, push 1

---
+++ Bai06/src/main/java/com/practice/maven/Calculator.java
@@ -0,0 +1,7 @@
+package com.practice.maven;
+
+public class Calculator {
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+}
---
+++ Bai06/src/test/java/com/practice/maven/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.practice.maven;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    public void testMultiply() {
+        Calculator calculator = new Calculator();
+        int result = calculator.multiply(4, 5);
+        assertEquals(20, result, "Loi: 4 x 5 phai bang 20!");
+    }
+}

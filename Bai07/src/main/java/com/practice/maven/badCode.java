3c68effd065730ba7c95ac01b4936b6097b2a78a
vhoang0502 <viethoang050207@gmail.com>
2026-05-06 22:50:47 +0700

    Test Bot Checkstyle

---
+++ Bai07/src/main/java/com/practice/maven/badCode.java
@@ -0,0 +1,10 @@
+package com.practice.maven;
+
+// Lỗi 1: Tên Class viết thường (Phải là BadCode)
+// Lỗi 2: Thiếu dấu cách ở toán tử
+public class badCode {
+    public void doSomething(){
+        int a=5;
+        System.out.println("Hello");
+    }
+}

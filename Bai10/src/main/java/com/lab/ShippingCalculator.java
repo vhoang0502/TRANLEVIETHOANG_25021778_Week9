7c7aab3157e795fdf51dfb79fc966fbfac6221ba
vhoang0502 <viethoang050207@gmail.com>
2026-05-07 19:20:09 +0700

    add Bai10

---
+++ Bai10/src/main/java/com/lab/ShippingCalculator.java
@@ -0,0 +1,13 @@
+package com.lab;
+
+public class ShippingCalculator {
+
+    public double calculate(double weight, String type) {
+        if (weight <= 0) {
+            throw new IllegalArgumentException("Weight must be positive");
+        }
+        if (type.equals("EXPRESS")) return weight * 5000 + 20000;
+        if (type.equals("STANDARD")) return weight * 3000;
+        throw new IllegalArgumentException("Unknown type: " + type);
+    }
+}
